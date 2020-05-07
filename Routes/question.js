f9b9167ac291aa8bd58177b6da9d1a848f2fa3a5
Rabin Gurung <48662958+rabinGurung@users.noreply.github.com>
2020-05-07 21:25:09 +0545

    fixed and cleaned Code

--- Routes/question.js
+++ Routes/question.js
@@ -1,6 +1,5 @@
 const Router = require('express').Router()
 const {addQuestion,findAllQuestion} =  require('../DB_Controller/QuestionController')
-var getIP = require('ipware')().get_ip;
 Router.post('/postnew',addQuestion)
 Router.get('/getques',findAllQuestion)
 
